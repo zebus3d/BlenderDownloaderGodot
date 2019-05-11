0d729feaa322d54030983a4a88fad0e32d9fc13e
SavMartin <sav2011@hotmail.com>
2019-05-11 02:17:05 +0100

    Arreglo del selector de hash

--- App.gd
+++ App.gd
@@ -16,41 +16,19 @@ var matchRuleLinux32 = ""
 var matchRuleWindow64 = ""
 var matchRuleWindow32 = ""
 var matchRuleMac64 = ""
-
+var linux64_hash = ""
+var linux32_hash = ""
+var win64_hash = ""
+var win32_hash = ""
+var mac64_hash = ""
 
 func getUrl():
 	print("obteniendo la url")
-	#print($OSSelector.text)
-	#print($SystemSelector.text)
-	var architecture = $SystemSelector.text
-	
-	# nombres de versiones:
-	#blender-2.80-427c75e4c20b-linux-glibc224-x86_64.tar.bz2
-	#blender-2.80-427c75e4c20b-linux-glibc224-i686.tar.bz2
-	#blender-2.80-6ef48b13186c-win64.zip
-	#blender-2.80-e2d04229c38b-win32.zip
-	#blender-2.80-3dc9da3a74ee-OSX-10.9-x86_64.zip
-	
-#	if $OSSelector.text == 'Linux':
-#		if architecture == 'x64':
-#			matchRule = ".*(blender-2.80-)([0-9a-zA-Z]*)(-linux-glibc224-x86_64.tar.bz2).*"
-#		elif architecture == 'x32':
-#			matchRule = ".*(blender-2.80-)([0-9a-zA-Z]*)(-linux-glibc224-i686.tar.bz2).*"
-#
-#	if $OSSelector.text == 'Windows':
-#		if architecture == 'x64':
-#			matchRule = ".*(blender-2.80-)([0-9a-zA-Z]*)(-win64.zip).*"
-#		elif architecture == 'x32':
-#			matchRule = ".*(blender-2.80-)([0-9a-zA-Z]*)(-win32.zip).*"
-#
-#	if $OSSelector.text == 'Mac':
-#		matchRule = ".*(blender-2.80-)([0-9a-zA-Z]*)(-OSX-10.9-x86_64.zip).*"
-	
+	matchRuleMac64 = ".*(blender-2.80-)([0-9a-zA-Z]*)(-OSX-10.9-x86_64.zip).*"
 	matchRuleLinux64 = ".*(blender-2.80-)([0-9a-zA-Z]*)(-linux-glibc224-x86_64.tar.bz2).*"
 	matchRuleLinux32 = ".*(blender-2.80-)([0-9a-zA-Z]*)(-linux-glibc224-i686.tar.bz2).*"
 	matchRuleWindow64 = ".*(blender-2.80-)([0-9a-zA-Z]*)(-win64.zip).*"
 	matchRuleWindow32 = ".*(blender-2.80-)([0-9a-zA-Z]*)(-win32.zip).*"
-	matchRuleMac64 = ".*(blender-2.80-)([0-9a-zA-Z]*)(-OSX-10.9-x86_64.zip).*"
 	
 	$HTTPRequest.request("https://builder.blender.org/download/")
 
@@ -59,6 +37,7 @@ func _disable_arch(pesel, condition):
 	$SystemSelector.disabled = condition
 
 func _ready():
+	getUrl()
 	match current_os:
 		"Windows":
 			$OSSelector.select(0)
@@ -77,14 +56,10 @@ func _ready():
 	# Add the date and extension to the file name to compare to previous versions
 	date = OS.get_date(true)
 	date = str(date['day']) + '-' + str(date['month']) + '-' + str(date['year'])
-	_update_file_name()
 	
 
 	$ProgressBar.value = 0
 	up_to_date = is_up_to_date()
-	
-	getUrl()
-	
 
 func is_up_to_date():
 	# Check if the daily zip already exists on our system
@@ -100,21 +75,25 @@ func is_up_to_date():
 func update_download_link():
 	var architecture = $SystemSelector.text
 	if blender_hash:
-		
 		if $OSSelector.text == 'Windows':
 			if architecture == 'x64':
-				download_link = 'https://builder.blender.org/download/blender-2.80-'+str(blender_hash)+'-win64.zip'	
+				print("Version Windows 64:"+win64_hash)
+				download_link = 'https://builder.blender.org/download/blender-2.80-'+str(win64_hash)+'-win64.zip'
 			else:
-				download_link = 'https://builder.blender.org/download/blender-2.80-'+str(blender_hash)+'-win32.zip'				
+				print("Version Windows 32:"+win32_hash)
+				download_link = 'https://builder.blender.org/download/blender-2.80-'+str(win32_hash)+'-win32.zip'
 		
 		elif $OSSelector.text == 'Mac':
-			download_link = 'https://builder.blender.org/download/blender-2.80-'+str(blender_hash)+'-OSX-10.9-x86_64.zip'
+			print("Version Windows MAC:"+mac64_hash)
+			download_link = 'https://builder.blender.org/download/blender-2.80-'+str(mac64_hash)+'-OSX-10.9-x86_64.zip'
 		
 		elif $OSSelector.text == 'Linux':
 			if architecture == 'x64':
-				download_link = 'https://builder.blender.org/download/blender-2.80-'+str(blender_hash)+'-linux-glibc224-x86_64.tar.bz2'
+				print("Version linux 64:"+linux64_hash)
+				download_link = 'https://builder.blender.org/download/blender-2.80-'+str(linux64_hash)+'-linux-glibc224-x86_64.tar.bz2'
 			else:
-				download_link = 'https://builder.blender.org/download/blender-2.80-'+str(blender_hash)+'-linux-glibc224-i686.tar.bz2'
+				print("Version linux 32:"+linux32_hash)
+				download_link = 'https://builder.blender.org/download/blender-2.80-'+str(linux32_hash)+'-linux-glibc224-i686.tar.bz2'
 		
 		print('Download link: ', download_link)
 		links_ready = true
@@ -137,9 +116,8 @@ func _on_OSSelector_item_selected(ID):
 		2:
 			file_ext = ".tar.bz2"
 			_disable_arch($SystemSelector.selected, false)
-	
-	getUrl()
-	#update_download_link()
+			
+	update_download_link()
 
 
 func _on_SystemSelector_item_selected(_ID):
@@ -192,7 +170,6 @@ func _process(_delta):
 	else:
 		$DownloadButton.disabled = true
 		
-#func _on_HTTPRequest_request_completed(result, response_code, headers, body):
 func _on_HTTPRequest_request_completed(result, response_code, _headers, _body):
 	
 	# si se hace click al boton descarga que no vuelva
@@ -212,12 +189,14 @@ func _on_HTTPRequest_request_completed(result, response_code, _headers, _body):
 			if regex_match:
 				blender_hash = regex_match.get_string(2)
 				matchrules[i] = regex_match.get_string(2)
-				print("match!")
-				print(blender_hash)
+				print("match! : " + matchrules[i])
+				linux64_hash = matchrules[0]
+				linux32_hash = matchrules[1]
+				win64_hash = matchrules[2]
+				win32_hash = matchrules[3]
+				mac64_hash = matchrules[4]
 			else:
-				print("no se encontro el regex")
-		
-		print(matchrules[1])
+				print("no se encontro el regex")		
 		if blender_hash:
 			update_download_link()
 	
@@ -228,7 +207,7 @@ func _on_HTTPRequest_request_completed(result, response_code, _headers, _body):
 		if current_os == "Windows":
 			# Unzip file
 			var _command = OS.execute("unzip.exe", [cwd + '/' + file_name, '-d', cwd], true)
-			OS.execute("mv", [cwd + '/godot.exe', cwd + '/godot-nightly.exe'], true)
+#			OS.execute("mv", [cwd + '/godot.exe', cwd + '/godot-nightly.exe'], true)
 			up_to_date = is_up_to_date()
 			# Open the dir
 			OS.shell_open(OS.get_user_data_dir())
